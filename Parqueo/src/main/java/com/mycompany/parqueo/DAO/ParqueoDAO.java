714acf2fa97ecf223b53dd06f217d481feebb3bf
Ahernandez2904 <hernandezlara180@gmail.com>
2021-04-29 07:03:15 -0600

    parqueo

--- Parqueo/src/main/java/com/mycompany/parqueo/DAO/ParqueoDAO.java
+++ Parqueo/src/main/java/com/mycompany/parqueo/DAO/ParqueoDAO.java
@@ -1,7 +1,16 @@
 package com.mycompany.parqueo.DAO;
 
+import com.mycompany.parqueo.Parqueo;
+import com.mycompany.parqueo.Conexion;
+import java.sql.CallableStatement;
 import java.sql.SQLException;
+import java.sql.Types;
+import javax.swing.JOptionPane;
 
 public class ParqueoDAO {
+    private Parqueo parqueo;
+    private Conexion conn;
+    
+    public ParqueoDAO() { }
     
 }
